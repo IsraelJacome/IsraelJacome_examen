34e3d4e4830ba1d135f620ba26c4fca20bc40100
ERMES_DMS <ERMES_DMS@PC>
2020-10-30 21:50:17 -0500

    Creacion proyecto

---
+++ IsraelJacome_examen/IsraelJacome_examen/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IsraelJacome_examen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirVentanaRegistro_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            string user_val = "estudiante2020";
+            string pass_val = "uisrael2020";
+
+            if (user == user_val && pass == pass_val)
+            {
+                await Navigation.PushAsync(new Registro(user));
+
+            }
+            else
+            {
+                await DisplayAlert("Ingreso", "Ingreso Iconrrecto", "ok");
+
+            }
+
+        }
+    }
+}
---
+++ IsraelJacome_examen/IsraelJacome_examen/Registro.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IsraelJacome_examen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string user)
+        {
+            InitializeComponent();
+            string usuar = lbluser.Text;
+            lbluser.Text = usuar + user;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            int montos = int.Parse(txtMontoInicial.Text);
+
+            if (montos != 1800)
+            {
+                var pagomensual = 1800 - montos;
+                var restocu = pagomensual / 3;
+                var porcentaje = restocu * 0.05;
+                var total = restocu + porcentaje;
+                txtMensual.Text = (total).ToString();
+                txtPagoR.Text = (total * 3).ToString();
+
+            }
+            else
+                txtPagoR.Text = "0";
+        }
+
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+
+                await DisplayAlert("Ingreso", "Ingreso Iconrrecto", "ok");
+            }
+
+            else
+            {
+                await DisplayAlert("Guardar", "Elemento Guardado con exito", "ok");
+                string usuario = lbluser.Text;
+                string nombre = txtNombre.Text;
+                string monto = txtPagoR.Text;
+                await Navigation.PushAsync(new Resumen(usuario, nombre, monto));
+
+            }
+
+        }
+
+    }
+
+       
+}
